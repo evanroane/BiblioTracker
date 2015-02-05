f03c05c422f4a4f424c8453df09c1f4ef6dc2b3b
evanroane <evan.roane@gmail.com>
2015-02-05 15:27:57 -0600

    added textboxes for collection stats zero state still passing

--- UITests/UITest.cs
+++ UITests/UITest.cs
@@ -30,28 +30,35 @@ namespace UITests
         [TestMethod]
         public void TestZeroState()
         {
-            //Get elements and store as local vars
+            //Get elements for local refrence
             ComboBox combo_categories = window.Get<ComboBox>("Combo_Categories");
-            //ComboBox item_all = window.Get<ComboBoxItem>("Item_All");
             Button button_view = window.Get<Button>("Button_View");
             Button button_add = window.Get<Button>("Button_Add");
             TextBox box_total_volumes = window.Get<TextBox>("Box_Total_Volumes");
             TextBox box_for_sale = window.Get<TextBox>("Box_For_Sale");
             TextBox box_sold = window.Get<TextBox>("Box_Sold");
-            //ListItem listItem = new Li
+            TextBox box_total_volumes_num = window.Get<TextBox>("Box_Total_Volumes_Num");
+            TextBox box_for_sale_num = window.Get<TextBox>("Box_For_Sale_Num");
+            TextBox box_sold_num = window.Get<TextBox>("Box_Sold_Num");
 
-            //State of elements at zero state
+            //Describbe state of individual elements at zero state
             Assert.IsTrue(button_view.Enabled);
             Assert.IsTrue(button_add.Enabled);
             Assert.IsTrue(combo_categories.Enabled);
             Assert.IsTrue(box_total_volumes.IsReadOnly);
             Assert.IsTrue(box_for_sale.IsReadOnly);
             Assert.IsTrue(box_sold.IsReadOnly);
+            Assert.IsTrue(box_total_volumes_num.IsReadOnly);
+            Assert.IsTrue(box_for_sale_num.IsReadOnly);
+            Assert.IsTrue(box_sold_num.IsReadOnly);
 
             //Contents of elements at zero state
             Assert.AreEqual(box_total_volumes.Text, "Total Volumes:");
             Assert.AreEqual(box_for_sale.Text, "For Sale:");
             Assert.AreEqual(box_sold.Text, "Sold:");
+            Assert.AreEqual(box_total_volumes_num.Text, "0");
+            Assert.AreEqual(box_for_sale_num.Text, "0");
+            Assert.AreEqual(box_sold_num.Text, "0");
             Assert.AreEqual(combo_categories.Item(0).Text, "All Items");
             Assert.AreEqual(combo_categories.Item(1).Text, "For Sale");
             Assert.AreEqual(combo_categories.Items[2].Text, "Sold");
