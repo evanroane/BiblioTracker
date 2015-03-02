866f5988a10e5a3783499c1bddcafb6886ea19f6
evanroane <evan.roane@gmail.com>
2015-03-02 09:44:46 -0600

    Edit Volume window updates db

--- BiblioTrack/EditVolume.xaml.cs
+++ BiblioTrack/EditVolume.xaml.cs
@@ -19,12 +19,29 @@ namespace BiblioTrack
     /// </summary>
     public partial class EditVolume : Window
     {
+        private Model.Volume VolumeToEdit;
         public EditVolume(Model.Volume V)
         {
             InitializeComponent();
+            VolumeToEdit = V;
             this.Edit_Author_First_Name.Text = V.authorFirstName;
             this.Edit_Author_Last_Name.Text = V.authorLastName;
             this.Edit_Volume_Title.Text = V.volumeTitle;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string auth_first_name = Edit_Author_First_Name.Text;
+            string auth_last_name = Edit_Author_Last_Name.Text;
+            string vol_title = Edit_Volume_Title.Text;
+            var mainWindow = Application.Current.Windows
+                .Cast<Window>()
+                .FirstOrDefault(window => window is MainWindow) as MainWindow;
+            mainWindow.repo.UpdateVolume(VolumeToEdit, auth_first_name, auth_last_name, vol_title);
+            this.Hide();
+            Edit_Author_First_Name.Text = "";
+            Edit_Author_Last_Name.Text = "";
+            Edit_Volume_Title.Text = "";
         }
 
     }
